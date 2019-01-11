e17e3da1da37d1146b2801a0bff6d8a88176a25a
Andrey Ivanov <andreymgn@protonmail.ch>
2019-01-12 01:13:47 +0300

    travis build docker image

--- cmd/RSOI-api/main.go
+++ cmd/RSOI-api/main.go
@@ -1,29 +1,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	portNum := flag.Int("port", -1, "Port on which service well listen")
-	postServerAddr := flag.String("post-server", "", "Address of post server")
-	commentServerAddr := flag.String("comment-server", "", "Address of comment server")
-	postStatsServerAddr := flag.String("post-stats-server", "", "Address of post stats server")
-	userServerAddr := flag.String("user-server", "", "Address of user server")
-	jaegerAddr := flag.String("jaeger-addr", "", "Jaeger address")
-
-	flag.Parse()
-
-	port := *portNum
-	ps := *postServerAddr
-	cs := *commentServerAddr
-	pss := *postStatsServerAddr
-	us := *userServerAddr
-	ja := *jaegerAddr
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		fmt.Println("PORT parse error")
+		return
+	}
+	postServerAddr := os.Getenv("POST-ADDR")
+	commentServerAddr := os.Getenv("COMMENT-ADDR")
+	postStatsServerAddr := os.Getenv("POSTSTATS-ADDR")
+	userServerAddr := os.Getenv("USER-ADDR")
+	jaegerAddr := os.Getenv("JAEGER-ADDR")
 
 	fmt.Printf("running API service on port %d\n", port)
-	err := runAPI(port, ps, cs, pss, us, ja)
+	err = runAPI(port, postServerAddr, commentServerAddr, postStatsServerAddr, userServerAddr, jaegerAddr)
 
 	if err != nil {
 		fmt.Printf("finished with error %v", err)
